bd7b053972c7e3f981adc2b2544f3d72a18f993a
Balazs Stegmar <stegmarb@gmail.com>
2017-06-15 16:09:39 +0200

    added rabbit mq

--- src/main/java/com/greenfox/controller/MainRestController.java
+++ src/main/java/com/greenfox/controller/MainRestController.java
@@ -1,14 +1,18 @@
 package com.greenfox.controller;
 
+import com.greenfox.model.Send;
 import com.greenfox.model.Test;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainRestController {
+  private Send send = new Send();
 
   @RequestMapping("/")
-  public Test test() {
+  public Test test() throws Exception {
+    send.send();
+    send.consume();
     return new Test("that shit!");
   }
 }
---
+++ src/main/java/com/greenfox/model/Send.java
@@ -0,0 +1,78 @@
+package com.greenfox.model;
+
+import com.rabbitmq.client.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Send {
+
+  private URI rabbitMqUrl;
+  private final static String QUEUE_NAME = "kryptonite";
+
+  public void send() throws Exception {
+    try {
+      rabbitMqUrl = new URI(System.getenv("RABBITMQ_BIGWIG_TX_URL"));
+    } catch(URISyntaxException e) {
+      e.getStackTrace();
+    }
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setUsername(rabbitMqUrl.getUserInfo().split(":")[0]);
+    factory.setPassword(rabbitMqUrl.getUserInfo().split(":")[1]);
+    factory.setHost(rabbitMqUrl.getHost());
+    factory.setPort(rabbitMqUrl.getPort());
+    factory.setVirtualHost(rabbitMqUrl.getPath().substring(1));
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.exchangeDeclare("log", "fanout");
+
+    channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+    String message = "Hello World!";
+    channel.basicPublish("log", QUEUE_NAME, null, message.getBytes("UTF-8"));
+    System.out.println(" [x] Sent '" + message + "'");
+
+    channel.close();
+    connection.close();
+  }
+
+  public void consume() throws Exception {
+    try {
+      rabbitMqUrl = new URI(System.getenv("RABBITMQ_BIGWIG_RX_URL"));
+    } catch(URISyntaxException e) {
+      e.getStackTrace();
+    }
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setUsername(rabbitMqUrl.getUserInfo().split(":")[0]);
+    factory.setPassword(rabbitMqUrl.getUserInfo().split(":")[1]);
+    factory.setHost(rabbitMqUrl.getHost());
+    factory.setPort(rabbitMqUrl.getPort());
+    factory.setVirtualHost(rabbitMqUrl.getPath().substring(1));
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.exchangeDeclare("log", BuiltinExchangeType.FANOUT, true);
+    channel.queueBind(QUEUE_NAME, "log", "kryptonite");
+
+    channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    Consumer consumer = new DefaultConsumer(channel) {
+      @Override
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+              throws IOException {
+        String message = new String(body, "UTF-8");
+        System.out.println(" [x] Received '" + message + "'");
+      }
+    };
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+  }
+}
